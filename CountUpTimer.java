dd868301e22ae2f04e4a4ac9f1a07daa489e865c
Essam <essamemad16@gmail.com>
2015-12-10 01:11:12 +0200

    Initial Commit.

---
+++ CountUpTimer.java
@@ -0,0 +1,82 @@
+package null;
+
+import android.os.CountDownTimer;
+
+/**
+ * Created by essam on 12/8/15.
+ */
+public class CountUpTimer {
+    public interface TimerDelegate {
+        public void onTick(long millisUntilFinished);
+
+        public void onFinish();
+    }
+
+    public CustomTimer(int end, int tick, final TimerDelegate delegate) {
+        this.delegate = delegate;
+        this.tick = tick;
+        // for some reason the last 2 ticks aren't counted
+        this.end = end + 2000;
+        timer = new CountDownTimer(end, tick) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeToGo = millisUntilFinished;
+                delegate.onTick(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                delegate.onFinish();
+            }
+        };
+    }
+    public void start(){
+        if(timer == null)
+            timer = new CountDownTimer(this.timeToGo, tick) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeToGo = millisUntilFinished;
+                    delegate.onTick(millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    delegate.onFinish();
+                }
+            };
+        this.timer.start();
+    }
+    public void pause(){
+        if(this.timer != null) {
+            this.timer.cancel();
+            this.timer = null;
+        }
+    }
+    public void restart(){
+        this.timer = new CountDownTimer(end,tick) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeToGo = millisUntilFinished;
+                delegate.onTick(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                delegate.onFinish();
+
+            }
+        };
+        start();
+    }
+
+    public long getTimeToGo(){
+        long returnValue = end - this.timeToGo;
+        return returnValue;
+    }
+
+    private CountDownTimer timer;
+    private TimerDelegate delegate;
+    private long tick;
+    private long timeToGo;
+    private int end;
+}
